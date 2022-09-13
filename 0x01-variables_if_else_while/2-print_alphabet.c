6c692ad6d865603d7ea48b6e745f73879b9cfe2f
Keeya5126 <111028095+Keeya5126@users.noreply.github.com>
2022-09-13 03:09:56 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	char kep;
+
+	for (kep = 'a'; kep <= 'z'; kep++)
+	{
+		putchar(kep);
+	}
+	putchar('\n');
+	return (0);
+}
