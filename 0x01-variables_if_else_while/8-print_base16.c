4e7a809feb227cf6e7927c74cba2fc0fff8de2a7
Keeya5126 <111028095+Keeya5126@users.noreply.github.com>
2022-09-13 03:23:19 +0300

    Create 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int vim;
+	char miv;
+
+	for (vim = 48; vim < 58; vim++)
+		putchar(sammy);
+	for (miv = 'a'; miv <= 'f'; miv++)
+		putchar(miv);
+	putchar('\n');
+	return (0);
+}
