f7baf05196ec5a975a34f248935d8100864b52bc
Keeya5126 <111028095+Keeya5126@users.noreply.github.com>
2022-09-13 03:15:33 +0300

    Create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * author: keeya
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	int p;
+
+	for (p = 0; p < 10; p++)
+	{
+		printf("%d", p);
+	}
+	printf("\n");
+	return (0);
+}
