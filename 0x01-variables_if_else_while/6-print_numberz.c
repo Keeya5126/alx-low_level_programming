a4e4f2487f26286cb3b977a974e982a037a07252
Keeya5126 <111028095+Keeya5126@users.noreply.github.com>
2022-09-13 03:19:52 +0300

    Create 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * author: kp
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int kp;
+
+	for (kp = 48; kp < 58; kp++)
+		putchar(kp);
+	putchar('\n');
+	return (0);
+}
